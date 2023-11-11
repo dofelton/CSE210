d8763c2ecf5b88ce6fa130f7d813f8b9ec66bb9b
dofelton <dofelton@gmail.com>
2023-11-11 17:44:55 -0600

    Journal Program

--- prove/Develop02/Entry.cs
+++ prove/Develop02/Entry.cs
@@ -1,9 +1,19 @@
 using System;
 
-public class entry
+public class Entry
 {
     public string _response;
     public DateTime _date;
     public string _prompt;
-    
+    public List <string> _prompts = new List<string>();
+    public void Display()
+    {
+
+    }
+
+    public void GetRandomPrompt()
+    {
+        
+    }
+
 }
